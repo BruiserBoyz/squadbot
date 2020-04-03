fb915c7134da8c911c7447d954e86ab0722b941d
jayarghargh <jr@williamreid.com.au>
2020-04-03 13:33:38 +1030

    Initial setup based on tpow's stuff

---
+++ cogs/Tester.py
@@ -0,0 +1,48 @@
+import random
+
+from discord.ext import commands
+
+
+class ShortCircuit():
+    def __init__(self):
+        self.message = "is alive"
+        self.players = []
+
+    def return_message(self):
+        return self.message
+
+    def add_player(self, playerName):
+        self.players.append(playerName)
+
+    def get_players(self):
+        return self.players
+
+
+class Tester(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+        self.robot = ShortCircuit()
+
+    # Tester
+    @commands.command(aliases=['jr'])
+    async def _jr(self, ctx, *, question):
+        responses = ['henlo, number five...' + self.robot.return_message() ,]
+        await ctx.send(f'you say: {question}\nI say: {random.choice(responses)}')
+
+    # Add player to array
+    @commands.command(aliases=['ap'])
+    async def _add_player(self, ctx, *, playerName):
+        self.robot.add_player(playerName)
+        await ctx.send(f'Player {playerName} added.')
+
+    # List players.
+    @commands.command(aliases=['lp'])
+    async def _list_players(self, ctx):
+        players = self.robot.get_players()
+        await ctx.send(f'Player {players}.')
+
+
+
+def setup(client):
+    client.add_cog(Tester(client))
---
+++ squadbot.py
@@ -0,0 +1,66 @@
+import os
+import discord
+
+# pip install discord.py
+# pip install python-dotenv
+# pip install pillow
+from discord.ext import commands
+from dotenv import load_dotenv
+
+project_folder = os.path.expanduser('~/Code/BruiserBoyz')  # adjust as appropriate
+load_dotenv(os.path.join(project_folder, '.env'))
+SECRET_KEY = os.getenv("CLIENT_ID")
+
+# client = discord.Client()
+# channel_id = 'DEFINE_ME'
+# token = 'DEFINE_ME'
+#
+# client = discord.Client()
+# channel = discord.Object(id=channel_id)
+
+client = commands.Bot(command_prefix=".bb", case_insensitive=True)
+
+# displays an online message when bot is online.
+@client.event
+async def on_ready():
+    # print('I have become self aware...')
+    # print('logged in as')
+    # print(client.user.name)
+    # print(client.user.id)
+    # print('-----')
+    await client.change_presence(activity=discord.Game(name="I am self-aware"))
+
+
+# cog load commands
+@client.command()
+async def load(ctx, extension):
+    """loads an extension eg. .load (extension)"""
+    client.load_extension(f'cogs.{extension}')
+    await ctx.send("The extension was loaded.")
+
+
+# cog unload commands
+@client.command()
+async def unload(ctx, extension):
+    """unloads an extension eg. .unload (extension)"""
+    client.unload_extension(f'cogs.{extension}')
+    await ctx.send("The extension was unloaded.")
+
+
+# cog reload commands
+@client.command()
+async def reload(ctx, extension):
+    """reloads an extension eg .reload (extension) """
+    client.unload_extension(f'cogs.{extension}')
+    client.load_extension(f'cogs.{extension}')
+    await ctx.send("The extension was reloaded.")
+
+
+# loads extensions (COGS) when on ready
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+
+
+# Run it.
+client.run(SECRET_KEY)
