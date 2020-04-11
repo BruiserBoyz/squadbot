b661e93a7655421d113efb44127b18cdb19be545
jayarghargh <jr@williamreid.com.au>
2020-04-11 16:13:39 +0930

    fixed files to load on container startup. Container in use is squadbot v0.03

--- nrun.sh
+++ nrun.sh
@@ -1,2 +1,9 @@
 #! /bin/bash
+#PATH_TO_MY_VENV=/home/justin/Code/BruiserBoyz/venv_bb/bin/
+PATH_TO_VENV=/venv_bb/bin/
+$PATH_TO_VENV/python -c 'import sys; print(sys.version_info)'
+python3 -c 'import sys; print(sys.version_info)'
+source /venv_bb/bin/activate
+cd /usr/sb/squadbot
+#ls
 nohup python3 squadbot.py &
