db3952d41cb5ef8427fab20aea523239b2628b88
jayarghargh <jr@williamreid.com.au>
2020-04-24 11:13:20 +0930

    added basic create wallet function.

--- cogs/bruiserBits.py
+++ cogs/bruiserBits.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import asyncio
 import asyncpg
 import datetime
+import discord
 
 # from .bruiser_bits.bb_accounts import MemberAccount
 # from .bruiser_bits.bb_bruiserbank import BruiserBank
@@ -11,6 +12,7 @@ import datetime
 #
 # # Create a new bank account for each new member.
 # this_bank.add_account(MemberAccount())
+SB_WALLET = "sb_wallet_1"
 CREDS = {
             "user": "postgres",
             "password": "cBUaCHS3ptvFyi",
@@ -31,16 +33,19 @@ class BruiserBits(commands.Cog):
         """Setup tables if they don't exist"""
         conn = await asyncpg.connect(**CREDS)
         # Execute a statement to create a new table.
+
+        # query = "INSERT INTO %s (col_1, col_2) VALUES (%%s, %%s)" % SB_WALLET
         await conn.execute('''
-                CREATE TABLE sb_wallet(
+                CREATE TABLE sb_wallet_2(
                     id serial PRIMARY KEY,
-                    name text,
-                    est date,
-                    bal double PRECISION
+                    d_user_id int UNIQUE,
+                    d_init_auth_id int,
+                    date_est date,
+                    balance double PRECISION
                 )
             ''')
 
-        self.loop.run_until_complete(self.fc_est_dc())
+        # self.loop.run_until_complete(self.fc_est_dc())
         await conn.close()
 
     # @commands.command(aliases=['qbal'])
@@ -51,22 +56,39 @@ class BruiserBits(commands.Cog):
     #     await ctx.send(x.get_balance())
 
     @commands.command(aliases=['qbal'])
-    async def get_db(self, ctx, *, user_nm):
+    async def get_db(self, ctx):
+        auth_id = ctx.message.author.id
         conn = await asyncpg.create_pool(**CREDS)
         self.row = await conn.fetchrow(
-            'SELECT bal FROM sb_wallet WHERE name = $1', user_nm
+            'SELECT balance FROM sb_wallet_2 WHERE d_user_id = $1', auth_id
         )
-        my_balance = self.row['bal']
+        my_balance = self.row['balance']
         await conn.close()
         await ctx.send(f'{BB}{my_balance}')
 
+
     @commands.command(aliases=['add_wallet'])
-    async def do_db(self, ctx, *, user_nm):
+    async def do_db(self, ctx, *, user: discord.User):
+        # Setup the variables.
+        now_date = datetime.datetime.now()
+        user_id = user.id
+        auth_id = ctx.message.author.id
         conn = await asyncpg.create_pool(**CREDS)
+        # Execute.
         await conn.execute('''
-                        INSERT INTO sb_wallet(name, est, bal) VALUES($1, $2, $3)
-                    ''', user_nm, datetime.date(2020, 4, 23), 0.0)
+                        INSERT INTO sb_wallet_2(d_user_id, d_init_auth_id, date_est, balance) VALUES($1, $2, $3, $4)
+                    ''', user_id, auth_id, datetime.date(now_date.year, now_date.month, now_date.day), 0.0)
         await conn.close()
+        print("wallet added")
+
+    # Add player to array
+    @commands.command(aliases=['check_user'])
+    async def _check_user(self, ctx, *, user: discord.User):
+
+        """for testing only"""
+        user_id = user.id
+        auth_id = ctx.message.author.id
+        print(user_id, auth_id)
 
     @commands.command(aliases=['add_money'])
     async def do_add_money(self, ctx, funds_to_add, user_nm):
@@ -81,20 +103,6 @@ class BruiserBits(commands.Cog):
                         ''', float(my_balance), user_nm)
         await ctx.send(f'{BB}{funds_to_add} added to account')
         await conn.close()
-    #
-    # @commands.command(aliases=['qdep'])
-    # async def _qdep(self, ctx):
-    #     """Very quick deposit to single account - qdep"""
-    #     counter = 0
-    #     for x in this_bank.get_all_accounts():
-    #         # print(x.get_balance)
-    #         this_bank.deposit(counter, 100)
-    #         counter += 1
-    #
-    # @commands.command(aliases=['tfr'])
-    # async def _tfr(self, ctx, *, account_from, account_to, amount):
-    #     """Transfer from a single account to another single account - tfr"""
-    #     this_bank.transfer(account_from, account_to, amount)
 
 
 def setup(client):
